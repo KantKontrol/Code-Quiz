55d8bd2773b6ca142a624f0d1bd97c7a26fd9e1b
KantKontrol <nick.derissio.tech@gmail.com>
2020-01-22 18:33:20 -0500

    added questions, there are now 10

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -28,10 +28,68 @@ var question3 = {
     used: false
 }
 
+var question4 = {
+    q: "Which of the following are capabilities of functions in JavaScript?",
+    ans : [ { choice: "1) Return a value", correct: false },
+            { choice: "2) Accept parameters and Return a value", correct: true },
+            { choice: "3) Accept parameters", correct: false},
+            { choice: "4) None of the above", correct: false}],
+    used: false
+}
+
+var question5 = {
+    q: "Which of the following is not a valid JavaScript variable name?",
+    ans : [ { choice: "1) 2names", correct: true },
+            { choice: "2) _first_and_last_names", correct: false },
+            { choice: "3) FirstAndLast", correct: false},
+            { choice: "4) None of the above", correct: false}],
+    used: false
+}
+
+var question6 = {
+    q: "What is the correct JavaScript syntax to write 'Hello World'?",
+    ans : [ { choice: "1) System.out.println('Hello World')", correct: false },
+            { choice: "2) println ('Hello World')", correct: false },
+            { choice: "3) document.write('Hello World')", correct: true},
+            { choice: "4) response.write('Hello World')", correct: false}],
+    used: false
+}
+
+var question7 = {
+    q: "Inside which HTML element do we put the JavaScript?",
+    ans : [ { choice: "1) <js>", correct: false },
+            { choice: "2) <scripting>", correct: false },
+            { choice: "3) <script>", correct: true},
+            { choice: "4) <javascript>", correct: false}],
+    used: false
+}
+
+var question8 = {
+    q: "Which of the following is not considered a JavaScript operator?",
+    ans : [ { choice: "1) new", correct: false },
+            { choice: "2) this", correct: true },
+            { choice: "3) delete", correct: false},
+            { choice: "4) typeof", correct: false}],
+    used: false
+}
+
+var question9 = {
+    q: "Which of the following is the structure of an if statement?",
+    ans : [ { choice: "1) if (conditional expression is true) thenexecute this codeend if", correct: false },
+            { choice: "2) if (conditional expression is true)execute this codeend if", correct: false },
+            { choice: "3) if (conditional expression is true)   {then execute this code}", correct: true},
+            { choice: "4) if (conditional expression is true) then {execute this code}", correct: false}],
+    used: false
+}
+
+var question10 = {
+    q: "The _______ method of an Array object adds and/or removes elements from an array.",
+    ans : [ { choice: "1) Reverse", correct: false },
+            { choice: "2) Shift", correct: false },
+            { choice: "3) Slice", correct: false},
+            { choice: "4) Splice", correct: true}],
+    used: false
+}
+
 var qArray = [];
-
-
-
-
-
 
